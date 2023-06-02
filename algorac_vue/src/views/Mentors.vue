6cd65578d86be0612a6c5077c8c8c7def63d3014
Dipraj paul <diprajpaul85@gmail.com>
2023-06-02 22:04:23 +0530

    Update Mentors.vue

--- algorac_vue/src/views/Mentors.vue
+++ algorac_vue/src/views/Mentors.vue
@@ -9,7 +9,6 @@
       </div>
       <img src="../assets/head.png" class="w-full" />
     </div>
-
     <div
       class="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-3"
     >
